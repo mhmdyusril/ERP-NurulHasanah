6f2275af24d699f4bac34449351c3adb7a3d6146
Muhamad Yusril <muh.yusril098@gmail.com>
2026-03-03 10:44:34 +0700

    style: set fixed page title RA Nurul Hasanah

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@isset($header) {{ strip_tags($header) }} - @endisset {{ config('app.name', 'ERP RA Nurul Hasanah') }}</title>
+        <title>RA Nurul Hasanah</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
